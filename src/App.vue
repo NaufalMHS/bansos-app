1f0e872322810f6a038119dedb6674a4e49d0a18
NaufalMHS <101965684+NaufalMHS@users.noreply.github.com>
2024-08-29 21:57:44 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Optional global styling */
+</style>
